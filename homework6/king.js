516ef366084f7ece57d444e7c0ddd2105ef7b047
locki13freja <111051887+locki13freja@users.noreply.github.com>
2022-09-01 23:44:18 +0300

    6lesson homework

---
+++ homework6/king.js
@@ -0,0 +1,6 @@
+{
+  let a=parseInt(prompt("Змінна a.Завдання 12"))
+  let b=parseInt(prompt("Змінна b.Завдання 12"))
+  let c=parseInt(prompt("Змінна c.Завдання 12"))
+  document.write(`Змінна a:${b}. <br/> Змінна b:${c}. <br/> Змінна c:${a}.`);
+}
